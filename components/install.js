ac10e2258b09bd33a0c7bf852563b5304c5d06d9
Gus <gdiazbanuelos@gmail.com>
2022-10-26 10:00:53 -0500

    Update install.js

--- components/install.js
+++ components/install.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 
-function Install(){
-    return(<>
+function Install() {
+    return (<>
         <div className="p-6 text-center drop-shadow text-white bg-[#FF9999]">
             <h1 className="font-bold mb-4 text-3xl">Install the bot</h1>
-            <Image src='/static/chill.gif' alt="Install" width="350px" height="300px"/>
-            
+            <Image src='/static/chill.gif' alt="Install" width="350px" height="300px" />
+
             <ol className='list-decimal mt-6 text-2xl space-y-2'>
                 <li>Download the <a href='https://github.com/gdiazbanuelos/Discord-IntroBot'
-                className='text-blue underline'>source code</a>
+                    target="_blank" rel="noreferrer noopener" className='text-blue underline'>source code</a>
                 </li>
                 <li>
                     Fill out the users.json file
